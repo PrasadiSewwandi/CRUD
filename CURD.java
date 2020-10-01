7780fd928ea7ba34e40454e9a7fe26ed38d17c6b
Prasadi Sewwandi <prasadi19943@gmail.com>
2020-10-02 00:24:54 +0530

    begun

---
+++ CURD.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class CURD {
+
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField2;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton deleteButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
